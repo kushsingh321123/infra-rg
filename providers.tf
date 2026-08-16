13f3abb162a22968a4005f4367db2a3a91016cfc
kushsingh321123 <kushsingh321123>
2026-08-16 08:10:36 +0530

    change

--- providers.tf
+++ providers.tf
@@ -1,15 +1,5 @@
 # terraform CI CS Excerise learning excerise2
 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "5.0.1"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
 }
 
