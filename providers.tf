ec79e44481218dfe0b32fcde4b77e452c6528ebe
kushsingh321123 <kushsingh321123>
2026-08-18 01:09:40 +0530

    Resource Group and storage account

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 # terraform CI CS Excerise learning excerise2
 terraform {
-  required_version = ">= 1.0.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 5.0.1"
     }
   }
 }
@@ -38,15 +38,15 @@ resource "azurerm_storage_account" "kdemo1s" {
 
 resource "azurerm_storage_account" "kdemo2s" {
   name                = "kdemo2s"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.KDEMO1R.name
+  location            = azurerm_resource_group.KDEMO1R.location
 
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
-  # depends_on = [
-  #   azurerm_resource_group.KDEMO1R
-  # ]
+  depends_on = [
+    azurerm_resource_group.KDEMO1R
+  ]
 
   tags = {
     Environment = "Development"
