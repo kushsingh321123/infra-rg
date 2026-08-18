7ff9acac070b41018e374b4e4b2bd42e393cec80
kushsingh321123 <kushsingh321123>
2026-08-18 10:44:56 +0530

    change

--- providers.tf
+++ providers.tf
@@ -15,42 +15,42 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "KDEMO1R" {
-  name     = "KDEMO1R"
-  location = "eastus"
+# resource "azurerm_resource_group" "KDEMO1R" {
+#   name     = "KDEMO1R"
+#   location = "eastus"
 
-  tags = {
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#   }
+# }
 
 
-resource "azurerm_storage_account" "kdemo1s" {
-  name                = "kdemo1s"
-  resource_group_name = azurerm_resource_group.KDEMO1R.name
-  location            = azurerm_resource_group.KDEMO1R.location
+# resource "azurerm_storage_account" "kdemo1s" {
+#   name                = "kdemo1s"
+#   resource_group_name = azurerm_resource_group.KDEMO1R.name
+#   location            = azurerm_resource_group.KDEMO1R.location
 
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
 
-  tags = {
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#   }
+# }
 
-resource "azurerm_storage_account" "kdemo2s" {
-  name                = "kdemo2s"
-  resource_group_name = azurerm_resource_group.KDEMO1R.name
-  location            = azurerm_resource_group.KDEMO1R.location
+# resource "azurerm_storage_account" "kdemo2s" {
+#   name                = "kdemo2s"
+#   resource_group_name = azurerm_resource_group.KDEMO1R.name
+#   location            = azurerm_resource_group.KDEMO1R.location
 
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  depends_on = [
-    azurerm_resource_group.KDEMO1R
-  ]
+#   depends_on = [
+#     azurerm_resource_group.KDEMO1R
+#   ]
 
-  tags = {
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#   }
+# }
