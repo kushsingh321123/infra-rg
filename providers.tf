1bad911b69b1f916215d3677d21f6faf0bf84fda
kushsingh321123 <kushsingh321123>
2026-08-18 22:21:53 +0530

    now updated

--- providers.tf
+++ providers.tf
@@ -11,9 +11,9 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
 #   resource "azurerm_resource_group" "KDEMO1R" {
 #   name     = "KDEMO1R"
