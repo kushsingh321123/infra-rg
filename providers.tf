ec6675125570280313aefd0f1b09e11955638614
kushsingh321123 <kushsingh321123>
2026-08-21 10:11:20 +0530

    Resolve providers Terraform configuration conflict

--- providers.tf
+++ providers.tf
@@ -66,25 +66,3 @@ resource "azurerm_storage_account" "kdemo1s" {
 #     Environment = "Development"
 #   }
 # }
-
-# resource "azurerm_storage_account" "kdemo3s" {
-#   name                = "kdemo3s"
-#   resource_group_name = azurerm_resource_group.KDEMO1R.name
-#   location            = azurerm_resource_group.KDEMO1R.location
-
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-
-#   network_rules {
-#     default_action = "Deny"
-#     bypass         = ["AzureServices"]
-#   }
-
-#   depends_on = [
-#     azurerm_resource_group.KDEMO1R
-#   ]
-
-#   tags = {
-#     Environment = "Development"
-#   }
-# }
